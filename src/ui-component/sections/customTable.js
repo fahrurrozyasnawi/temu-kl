718cabcfc07e50968c099349d6a67d426b488cdf
fahrurrozy <fahrurrozy.asnawi@gmail.com>
2023-12-25 00:06:31 +0800

    add pagination

--- src/ui-component/sections/customTable.js
+++ src/ui-component/sections/customTable.js
@@ -1,8 +1,10 @@
+import PropTypes from 'prop-types';
 import React, { Fragment, useMemo, useState } from 'react';
 import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   getSortedRowModel,
   SortingState,
   useReactTable
@@ -15,27 +17,60 @@ import {
   TableCell,
   TableContainer,
   TableHead,
+  TablePagination,
   TableRow
 } from '@mui/material';
 
 // third-party
 import PerfectScrollbar from 'react-perfect-scrollbar';
 
-const CustomTable = ({ data, columns }) => {
+const CustomTable = ({
+  data,
+  columns,
+  useServerSidePagination = false,
+  pagination,
+  pageCount = -1,
+  onPaginationChange
+}) => {
   const tableData = useMemo(() => data, [data]);
   const columnData = useMemo(() => columns, [columns]);
 
+  let options = {
+    getPaginationRowModel: getPaginationRowModel(),
+    pageCount: tableData.length
+  };
+
+  if (useServerSidePagination) {
+    options = {
+      pageCount: totalPages,
+      onPaginationChange,
+      manualPagination: true,
+      pageCount
+    };
+  }
+
   const [sorting, setSorting] = useState([]);
-  const { getHeaderGroups, getRowModel } = useReactTable({
+  const {
+    getHeaderGroups,
+    getRowModel,
+    getState,
+    setPageIndex,
+    setPageSize,
+    getPageCount
+  } = useReactTable({
     data: tableData,
     columns: columnData,
     state: {
-      sorting
+      sorting,
+      ...(useServerSidePagination && {
+        pagination
+      })
     },
     onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel()
+    ...options
   });
+
   return (
     <Card sx={{ p: 2 }}>
       <TableContainer>
@@ -119,8 +154,30 @@ const CustomTable = ({ data, columns }) => {
           </Table>
         </PerfectScrollbar>
       </TableContainer>
+
+      <TablePagination
+        component="div"
+        rowsPerPageOptions={[5, 10, 15]}
+        count={getPageCount()}
+        rowsPerPage={getState().pagination.pageSize}
+        page={getState().pagination.pageIndex}
+        onPageChange={(_, page) => setPageIndex(page)}
+        onRowsPerPageChange={(e) => {
+          const size = e.target.value ? Number(e.target.value) : 10;
+          setPageSize(size);
+        }}
+      />
     </Card>
   );
+};
+
+CustomTable.propTypes = {
+  data: PropTypes.array.isRequired,
+  columns: PropTypes.array.isRequired,
+  useServerSidePagination: PropTypes.bool,
+  pageCount: PropTypes.number,
+  pagination: PropTypes.object,
+  onPaginationChange: PropTypes.any
 };
 
 export default CustomTable;
